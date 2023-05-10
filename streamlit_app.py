1f0841c2a1863166470474190f2cd1a24c7ad0af
Francis Murray <21000873+francis-murray@users.noreply.github.com>
2023-05-10 17:46:39 +0200

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
+
+streamlit.text('Blueberry Oatmeal')
+streamlit.text('Kale, Spinach')
+streamlit.text('Hard-Boiled Egg')
