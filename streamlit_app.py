e2147b71034078cc9caa0d1cc272812d9b1934f1
Francis Murray <21000873+francis-murray@users.noreply.github.com>
2023-05-11 08:34:34 +0200

    Update streamlit_app.py
    Pull the data into a pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,14 @@
 import streamlit
+import pandas
+
 
 streamlit.title('My Parents New Healthy Diner')
-
 streamlit.header('Breakfast Menu')
-
 streamlit.text('🥣 Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach')
 streamlit.text('🐔 Hard-Boiled Egg')
-
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
