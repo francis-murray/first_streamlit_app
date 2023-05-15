f215330cb02d099363acabebdcd273ac756c7517
Francis Murray <21000873+francis-murray@users.noreply.github.com>
2023-05-15 15:43:14 +0200

    Update streamlit_app.py
    organize imports=

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
-
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -21,7 +23,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
-import requests
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
@@ -34,7 +35,6 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 
-import snowflake.connector
 
 # Query Our Trial Account Metadata 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
