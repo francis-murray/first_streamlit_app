85e34ee44f8e5f323628d65d29e2b3e4f24cf9d5
Francis Murray <21000873+francis-murray@users.noreply.github.com>
2023-05-15 15:41:57 +0200

    Update streamlit_app.py
    Add a STOP Command to Focus Our Attention

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,8 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 
+streamlit.stop()
+
 add_my_fruit = streamlit.text_input('What fruit would you like to add','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
